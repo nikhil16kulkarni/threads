0b63920c427165708d59dbdbd646b577f8cd5a1e
nikhil16kulkarni <nikhil16kulkarni@gmail.com>
2023-09-04 22:48:47 -0400

    Created profile section, tried to comment with another user, fixed profile link

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,12 +3,13 @@ import {sidebarLinks} from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
 import {usePathname, useRouter} from "next/navigation"
-import {SignedIn, SignOutButton, OrganizationSwitcher} from "@clerk/nextjs"
+import {SignedIn, SignOutButton, OrganizationSwitcher, useAuth} from "@clerk/nextjs"
 
 function LeftSidebar() {
 
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
 
     return (
         <section className="custom-scrollbar leftsidebar">
@@ -16,6 +17,8 @@ function LeftSidebar() {
                 {sidebarLinks.map((link) => {
 
                     const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+                    if(link.route === "/profile") link.route = `${link.route}/${userId}`
 
                     return (
                         <Link href={link.route} key={link.label} className= {`leftsidebar_link  ${isActive && "bg-primary-500"}`}>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { connectToDB } from "../mongoose"
 import User from "../models/user.model"
 import { revalidatePath } from "next/cache";
 import { error } from "console";
+import Thread from "../models/thread.model";
 
 interface Params {
     userId: string,
@@ -73,5 +74,34 @@ export async function fetchUser(userId: string){
         // })
     } catch(error: any) {
         throw new Error(`Failed to fetch user: ${error.message}`)
+    }
+}
+
+
+export async function fetchUserPosts(userId:string) {
+    try{
+        connectToDB();
+
+        const threads = await User.findOne({id: userId})
+        .populate({
+            path: "threads",
+            model: Thread,
+            populate:{
+                path: "children",
+                model: Thread,
+                populate: {
+                    path: "author",
+                    model: User,
+                    select: "name image id"
+                }
+            }
+        })
+
+
+        return threads;
+
+
+    } catch(error: any){
+        throw new Error(`Failed to fetch Threads: ${error.message}`)
     }
 }
